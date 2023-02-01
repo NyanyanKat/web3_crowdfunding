b9763b3c8442d9ca55b07cadcd2ef308961bf87d
Jimmy Chan <jchan85@gatech.edu>
2023-02-01 00:03:16 -0800

    Campaign details

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
         <CustomButton
           btnType="button"
           title={address ? 'Create a campaign' : 'Connect'}
-          styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6d5d]'}
+          styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
           handleClick={() => {
             if (address) navigate('create-campaign');
             else connect();
